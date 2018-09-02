86b4a4a31e911a339dc4e9caa394d2ffb6dacbb9
Administrator <Administrator@O3TBHFTLW5AF0GM.lan>
2018-09-02 21:17:00 +0800

    静态页

---
+++ phoibe-doc-cms-html/js/docdetail.js
@@ -0,0 +1,140 @@
+var baseUrl = "http://47.93.62.169:8090";
+//"http://192.168.199.139:8090";//"
+var totalRows = 0;
+var currPage = 1;
+var tid = getUrlString("tid");
+function getInfo() {
+
+    var url = baseUrl + "/phoibe/document/fetch/" + tid;
+    //alert(url);
+    $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        dataType: 'json',
+        success: function (result) {
+            if (result.code = "SUCCESS") {
+                if (result.data.format == "doc" || result.data.format == "docx") {
+                    $("#icontitle").attr("class", "doc");
+                }
+                if (result.data.format == "pdf") {
+                    $("#icontitle").attr("class", "pdf");
+                }
+                $("#date").html(result.data.createTime);
+                $("#format").html(result.data.format);
+                $("#size").html(result.data.fileSize);
+                $("#owner").html(result.data.userRealName);
+                $("#doctitle").html(result.data.name);
+                var desc = result.data.description;
+                if(desc!="undefined" && desc!="")
+                $("#doc-content").html("<p>"+desc+"</p>");
+            }
+        }
+    });
+}
+
+function loadData(pageindex) {
+    $("#comm-content").children().remove();
+    var docid = getUrlString("tid");
+    var url = baseUrl + "/phoibe/comment/list/"+docid+"/"+pageindex+"/10";
+            $.ajax({
+                type: 'GET',
+                url: url,
+                async: false,
+                dataType: 'json',
+                success: function (result) {
+                    var total_rows = result.data.totalCount;
+                    totalRows = total_rows;
+                 
+                    $.each(result.data.dataList, function (i, val) {
+                        var content = val["commentContent"];
+                        var createTime = val["createTime"];
+                        var title = $("#doctitle").html();
+                        var name = "admin";
+                        var step = 0;
+                        var row = "";
+                        var row = "<div class='row'><div class='imghead'><img src='images/head.png' /></div><span class='name'>" + name + "</span><span class='com-date'>"+createTime+"</span><br /><span class='doctitle'>《" + title + "》</span><span id='commconten'>"+content+"</span></div>";
+                        //alert(row);
+                        $("#comm-content").append(row);
+                    })
+                }
+            });
+
+            layui.use(['laypage', 'layer'], function () {
+                var laypage = layui.laypage
+                , layer = layui.layer;
+                laypage.render({
+                    elem: 'notice_pages'
+                  , count: totalRows
+                  , curr: currPage//当前页,
+                  , first: '首页'
+                  , last: '尾页'
+                  , prev: '<em>←</em>'
+                  , next: '<em>→</em>'
+                   , jump: function (obj, first) { //触发分页后的回调
+                       if (!first) { //点击跳页触发函数自身，并传递当前页：obj.curr
+                           currPage = obj.curr;
+                           loadData(obj.curr);
+                       }
+                   }
+                });
+            });
+}
+
+function initstar() {
+    var num = finalnum = tempnum = 0;
+    var lis = document.getElementsByTagName("li");
+    //num:传入点亮星星的个数
+    //finalnum:最终点亮星星的个数
+    //tempnum:一个中间值
+    function fnShow(num) {
+        finalnum = num || tempnum;//如果传入的num为0，则finalnum取tempnum的值
+        for (var i = 0; i < lis.length; i++) {
+            lis[i].className = i < finalnum ? "light" : "";//点亮星星就是加class为light的样式
+        }
+    }
+    for (var i = 1; i <= lis.length; i++) {
+        lis[i - 1].index = i;
+        lis[i - 1].onmouseover = function () { //鼠标经过点亮星星。
+
+            fnShow(this.index);//传入的值为正，就是finalnum
+        }
+        lis[i - 1].onmouseout = function () { //鼠标离开时星星变暗
+            fnShow(0);//传入值为0，finalnum为tempnum,初始为0
+        }
+        lis[i - 1].onclick = function () { //鼠标点击,同时会调用onmouseout,改变tempnum值点亮星星
+            tempnum = this.index;
+            $("#comentscore").val(tempnum);
+        }
+    }
+}
+$(function () {
+    initstar();
+        getInfo();
+        loadData(1);
+
+        $(".download").click(function () {
+            url = baseUrl + "/phoibe/document/download?Id="+tid;
+            window.location.href = url;
+        });
+        $("#submit").click(function () {
+            var url = baseUrl+"/phoibe/comment/save";
+            var content = $("#comment-content").val();
+            var score = $("#comentscore").val();
+            var url = url + "?documentId="+tid+"&commentContent="+content+"&score="+score+"&userId=1";
+            $.ajax({
+                type: 'POST',
+                url: url,
+                async: false,
+                dataType: 'json',
+                success: function (result) {
+                    if (result.code == "SUCCESS") {
+                        alert("评论成功");
+                        loadData(1);
+                    }
+                }
+            });
+        });
+});   
+
+      
---
+++ phoibe-doc-cms-html/js/stockmgr.js
@@ -0,0 +1,174 @@
+
+var totalRows = 0;
+var currPage = 1;
+var baseUrl = "http://47.93.62.169:8090";//"http://192.168.199.139:8090";
+//
+
+        function loadData(pageindex) {
+            $("#tblist-body").children().remove();
+
+            var data = baseUrl+'/phoibe/document/list/'+pageindex+'/10?1=1';
+
+            var docname = $("#docname").val();
+            var owner = $("#owner").val();
+            var chkValue = $("#con-value li[checked='checked']");
+            var doctypestatus = $("#doctype option:selected").val();
+            
+            var stockTimeBegin = startdate.value ;
+            var stockTimeEnd = enddate.value;
+
+            if (doctypestatus>0 && doctypestatus != null) {
+                data = data + "&auditStatus=" + doctypestatus.toLowerCase();
+            }
+
+            if (name != null);
+            data = data + "&name=" + docname;
+            if (owner != "") {
+                data = data + "&userRealName=" + owner;
+            }
+
+            var chkValue = $("#con-value li[checked='checked']");
+            var doctypevalue = chkValue.html();
+
+            if (doctypevalue != "undefined" && doctypevalue != null) {
+                data = data + "&format=" + doctypevalue.toLowerCase();
+            }
+
+            if (stockTimeBegin != "" && stockTimeBegin != null) {
+                data = data + "&stockTimeBegin=" + stockTimeBegin;
+            }
+
+            if (stockTimeEnd != "" && stockTimeEnd != null) {
+                data = data + "&stockTimeEnd=" + stockTimeEnd;
+            }
+            $.ajax({
+                type: 'GET',
+                url: data,
+                async: false,
+                dataType: 'json',
+                success: function (result) {
+                    var total_rows = result.data.totalCount;
+                    totalRows = total_rows;
+                    var step = 0;
+                    var row = "";
+                    $.each(result.data.dataList, function (i, val) {
+                        var title = val["name"];
+                        var id = val["id"];
+                        var pagecount = val["pagecount"];
+                        var filesize = val["fileSize"];
+                        var status = val["status"];
+                        var stockTime = val["stockTime"];
+                        var docstockstatus = val["isstock"];
+                        var owner = "admin";
+                        var auditdate = "2018-08-26";
+                        var auditor = "admin";
+                        var tag = "";
+                        var docstatus = "";
+                        var auditstatustyle = "f-blue";
+                        if (status == 1) {
+                            docstatus = "上传中";
+                        }
+
+                        else if (status == 2) {
+                            docstatus = "上传完成";
+                        }
+                        var docstockstyle="";
+                        if (docstockstatus == 1) {
+                            docstockstatus = "已入库";
+                            docstockstyle = "f-blue";
+                        }
+                        else {
+                            docstockstatus= "未入库";
+                            docstockstyle = "f-red";
+                        }
+                        var row = "<tr><td class='row-id'>" + id + "</td><td><input type='radio' name='chksel' data-value='" + id + "'/></td><td title='" + title + "'><a href='docdetail.html?tid=" + id + "'>" + title + "</a></td><td>" + filesize + "</td><td>" + owner + "</td><td>" + auditdate + "</td><td class='" + auditstatustyle + "'>" + stockTime + "</td><td class='" + docstockstyle + "'>" + docstockstatus + "</td><td>" + auditor + "</td></tr>";
+                        $("#tblist-body").append(row);
+                    });
+                }
+            });
+
+             layui.use(['laypage', 'layer'], function () {
+         var laypage = layui.laypage
+         , layer = layui.layer;
+
+         laypage.render({
+             elem: 'notice_pages'
+           , count: totalRows
+           ,curr:currPage
+           , first: '首页'
+           , last: '尾页'
+           , prev: '<em>←</em>'
+           , next: '<em>→</em>'
+            , jump: function (obj, first) { 
+                if (!first) { 
+                    currPage = obj.curr;
+                    loadData(obj.curr);
+                }
+            }
+         });
+     });
+        }
+
+        $(function () {
+            laydate.render({
+                elem: '#startdate'
+                    , done: function (value, date, endDate) {
+                        $("#startdate").attr("date-value", value);
+                    }
+
+            });
+            laydate.render({
+                elem: '#enddate'
+                        , done: function (value, date, endDate) {
+                            $("#enddate").attr("date-value", value);
+                        }
+            });
+
+            loadData(1);
+
+            $("#btnaddstock").click(function () {
+                var sel = $("#tblist-body tr td input[type='radio']:checked");
+                var rowid = $(sel).attr("data-value");
+                var data = baseUrl+'/phoibe/document/update/instorage/' + rowid;
+                $.ajax({
+                    type: 'GET',
+                    url: data,
+                    dataType: 'json',
+                    async: false,
+                    success: function (result) {
+                        if (result.code == "SUCCESS");
+                        {
+                            alert("文档入库成功");
+                            loadData(0);
+                        }
+                    }
+                });
+
+                
+            });
+            $("#btndelstock").click(function () {
+                var sel = $("#tblist-body tr td input[type='radio']:checked");
+                var rowid = $(sel).attr("data-value");
+
+                var data = baseUrl+'/phoibe/document/update/outstorage/' + rowid;
+                $.ajax({
+                    type: 'GET',
+                    url: data,
+                    dataType: 'json',
+                    async: false,
+                    success: function (result) {
+                        if (result.code == "SUCCESS");
+                        {
+                            alert("删除入库文档");
+                            loadData(0);
+                        }
+                    }
+                });
+            });
+
+            $("#btnSearch").click(function () {
+                currPage = 1;
+                totalRows = 0;
+                loadData(1);
+            });
+        });
